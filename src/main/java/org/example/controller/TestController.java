a7439eb457cc91c719f59c433ba82450907bb652
Mark Clifford <mark.clifford229@gmail.com>
2024-12-05 14:32:46 +0000

    Test

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
